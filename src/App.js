5a964a100e6d0db3ed3b74496f92ab9eaa0a0059
maniSHarma7575 <hacker.sharma7575@gmail.com>
2020-05-06 20:13:02 +0530

    Backend Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react'
 
+
+import {Cards,Charts,CountryPicker} from './components'
+import styles from './App.module.css'
+import {fetchData} from './api'
 class App extends React.Component{
+    state={
+        data:{}
+    }
+    async componentDidMount()
+    {
+        const fetchRecord=await fetchData();
+        this.setState({data:fetchRecord})
+        console.log(fetchRecord)
+
+    }
     render()
     {
+        const {data}=this.state
         return(
-            <div>
-                <h1>Hello manish app is working fine</h1>
+            <div className={styles.container}>
+                <Cards data={data}/>
+                <CountryPicker />
+                <Charts />
+                
             </div>
         )
     }
